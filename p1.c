169b97aa48567d921858c66d01ac97a1689e3e10
hobbit29 <104728293+hobbit29@users.noreply.github.com>
2022-08-26 12:51:24 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -1,21 +1,41 @@
 #include<stdio.h>
+
 int main(){
+
     int i1, i2;
+
     float f1, f2;
-    printf("Enter the first integer %d", i1);
+
+    printf("\nEnter the first integer: ");
+
     scanf("%d", &i1);
+
     printf("\nThe first entered integer is: %d", i1);
+
     
-    printf("Enter the second integer %d", i2);
+
+    printf("\nEnter the second integer: ");
+
     scanf("%d", &i2);
+
     printf("\nThe second entered integer is: %d", i2);
+
     
-    printf("Enter the first float %f", f1);
+
+    printf("\nEnter the first float: ");
+
     scanf("%f", &f1);
+
     printf("\nThe first entered float is: %f", f1);
+
     
-    printf("Enter the second float %f", f2);
+
+    printf("\nEnter the second float: ");
+
     scanf("%f", &f2);
+
     printf("\nThe second entered float is: %f", f2);
+
     return 0;
+
 }
